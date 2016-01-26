fca6ccdc269faa1eee9b15fb7a5202238fbfbf43
gmittal <gautam@mittal.net>
2016-01-25 20:10:50 -0800

    woo, getting somewhere

--- text-illustrate.py
+++ text-illustrate.py
@@ -1,16 +1,27 @@
-from textblob import TextBlob
+# AAR Natural Language Processing Project 2015-2016
+# Simple program that takes plaintext as input and illustrates interpretation
+# Written by Gautam Mittal
+# Mentor: Robert Cheung
+
+from textblob import TextBlob as tb
+from textblob import Word as word
+
 text = '''
-The titular threat of The Blob has always struck me as the ultimate movie
-monster: an insatiably hungry, amoeba-like mass able to penetrate
-virtually any safeguard, capable of--as a doomed doctor chillingly
-describes it--"assimilating flesh on contact.
-Snide comparisons to gelatin be damned, it's a concept with the most
-devastating of potential consequences, not unlike the grey goo scenario
-proposed by technological theorists fearful of
-artificial intelligence run rampant.
+The quick brown fox jumped over the lazy dog. He was very good.
 '''
+storyText = tb(text)
 
-blob = TextBlob(text)
+# does simple relational analysis
+def analyze_semantics(sentenceBlob): # requires tb(STRING) to be passed
+    # make sure the string is in English
+    if sentenceBlob.detect_language() != "en":
+        sentenceBlob = sentenceBlob.translate(to="en")
+    # print(sentenceBlob)
 
-for sentence in blob.sentences:
-    print(sentence.sentiment.polarity)
+    for w in range(0, len(sentenceBlob.words)):
+        lemmWord = sentenceBlob.words[w].lemmatize("v")
+        print(lemmWord)
+
+# breaks the large text up into blob sized chunks
+for sentence in storyText.sentences:
+    analyze_semantics(sentence)
