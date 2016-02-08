05c25fe8f0540af6de8a05e8421ecf774beffc61
gmittal <gautam@mittal.net>
2016-02-07 17:36:31 -0800

    fixes

--- text-illustrate.py
+++ text-illustrate.py
@@ -2,21 +2,92 @@
 # Simple program that takes plaintext as input and illustrates interpretation
 # Written by Gautam Mittal
 # Mentor: Robert Cheung
-# Requires NLTK and its respective corpora
+# Requires NLTK and its respective corpora and MegaM
 
 import re
 import nltk
 from nltk import CFG, ChartParser, RegexpParser
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, conll2000
 from textblob import TextBlob as tb
 from textblob import Word as word
 from textblob.parsers import PatternParser
 from contractions import *
+train_sents = conll2000.chunked_sents('train.txt', chunk_types=['NP'])
+test_sents = conll2000.chunked_sents('test.txt', chunk_types=['NP'])
+
 
 text = '''
-The quick brown fox jumped over the lazy dog.
+The quick brown fox decided to jump over the lazy dog.
 '''
 storyText = tb(text)
+
+
+def npchunk_features(sentence, i, history):
+    word, pos = sentence[i]
+    if i == 0:
+        prevword, prevpos = "<START>", "<START>"
+    else:
+        prevword, prevpos = sentence[i-1]
+    if i == len(sentence)-1:
+        nextword, nextpos = "<END>", "<END>"
+    else:
+        nextword, nextpos = sentence[i+1]
+    return {"pos": pos,
+            "word": word,
+            "prevpos": prevpos,
+            "nextpos": nextpos,
+            "prevpos+pos": "%s+%s" % (prevpos, pos),
+            "pos+nextpos": "%s+%s" % (pos, nextpos),
+            "tags-since-dt": tags_since_dt(sentence, i)}
+
+def tags_since_dt(sentence, i):
+    tags = set()
+    for word, pos in sentence[:i]:
+        if pos == 'DT':
+            tags = set()
+        else:
+            tags.add(pos)
+    return '+'.join(sorted(tags))
+
+class ConsecutiveNPChunkTagger(nltk.TaggerI):
+
+    def __init__(self, train_sents):
+        train_set = []
+        for tagged_sent in train_sents:
+            untagged_sent = nltk.tag.untag(tagged_sent)
+            history = []
+            for i, (word, tag) in enumerate(tagged_sent):
+                featureset = npchunk_features(untagged_sent, i, history)
+                train_set.append( (featureset, tag) )
+                history.append(tag)
+        self.classifier = nltk.MaxentClassifier.train(
+            train_set, trace=0)
+
+    def tag(self, sentence):
+        history = []
+        for i, word in enumerate(sentence):
+            featureset = npchunk_features(sentence, i, history)
+            tag = self.classifier.classify(featureset)
+            history.append(tag)
+        return zip(sentence, history)
+
+
+
+class ConsecutiveNPChunker(nltk.ChunkParserI):
+    def __init__(self, train_sents):
+        tagged_sents = [[((w,t),c) for (w,t,c) in
+                         nltk.chunk.tree2conlltags(sent)]
+                        for sent in train_sents]
+        self.tagger = ConsecutiveNPChunkTagger(tagged_sents)
+
+    def parse(self, sentence):
+        tagged_sents = self.tagger.tag(sentence)
+        conlltags = [(w,t,c) for ((w,t),c) in tagged_sents]
+        return nltk.chunk.conlltags2tree(conlltags)
+
+
+
+
 
 def tokenize(string):
     string = str(string.replace("\n", ""))#.replace(".", ""))
@@ -40,16 +111,21 @@ def prepare_text(stringBlob):
 
 
 def analyze_semantics(sentenceBlob):
-    grammar_model = r"""
-      NP: {<DT|JJ|NN.*>+}          # Chunk sequences of DT, JJ, NN
-      PP: {<IN><NP>}               # Chunk prepositions followed by NP
-      VP: {<VB.*><NP|PP|CLAUSE>+$} # Chunk verbs and their arguments
-      CLAUSE: {<NP><VP>}           # Chunk NP, VP
-      """
-    tagged_s = tb(" ".join(prepare_text(sentenceBlob))).tags
-    parser = nltk.RegexpParser(grammar_model)
-    structure_tree = parser.parse(tagged_s) # sentence structure tree
-    return structure_tree
+    # grammar_model = r"""
+    #   NP: {<DT|JJ|NN.*>+}          # Chunk sequences of DT, JJ, NN
+    #   PP: {<IN><NP>}               # Chunk prepositions followed by NP
+    #   VP: {<VB.*><NP|PP|CLAUSE>+$} # Chunk verbs and their arguments
+    #   CLAUSE: {<NP><VP>}           # Chunk NP, VP
+    #   """
+    # tagged_s = tb(" ".join(prepare_text(sentenceBlob))).tags
+    # parser = nltk.RegexpParser(grammar_model)
+    # structure_tree = parser.parse(tagged_s) # sentence structure tree
+    # structure_tree.draw()
+    # return structure_tree
+
+
+    chunker = ConsecutiveNPChunker(train_sents)
+    print(chunker.evaluate(test_sents))
 
 def main():
     for sentence in storyText.sentences: # split text into sentences
