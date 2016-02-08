fd965a02676d5847a1abc7622ec8c8644b5e8e29
gmittal <gautam@mittal.net>
2016-02-07 18:29:36 -0800

    unigram chunker

--- text-illustrate.py
+++ text-illustrate.py
@@ -22,70 +22,19 @@ The quick brown fox decided to jump over the lazy dog.
 storyText = tb(text)
 
 
-def npchunk_features(sentence, i, history):
-    word, pos = sentence[i]
-    if i == 0:
-        prevword, prevpos = "<START>", "<START>"
-    else:
-        prevword, prevpos = sentence[i-1]
-    if i == len(sentence)-1:
-        nextword, nextpos = "<END>", "<END>"
-    else:
-        nextword, nextpos = sentence[i+1]
-    return {"pos": pos,
-            "word": word,
-            "prevpos": prevpos,
-            "nextpos": nextpos,
-            "prevpos+pos": "%s+%s" % (prevpos, pos),
-            "pos+nextpos": "%s+%s" % (pos, nextpos),
-            "tags-since-dt": tags_since_dt(sentence, i)}
-
-def tags_since_dt(sentence, i):
-    tags = set()
-    for word, pos in sentence[:i]:
-        if pos == 'DT':
-            tags = set()
-        else:
-            tags.add(pos)
-    return '+'.join(sorted(tags))
-
-class ConsecutiveNPChunkTagger(nltk.TaggerI):
-
+class UnigramChunker(nltk.ChunkParserI):
     def __init__(self, train_sents):
-        train_set = []
-        for tagged_sent in train_sents:
-            untagged_sent = nltk.tag.untag(tagged_sent)
-            history = []
-            for i, (word, tag) in enumerate(tagged_sent):
-                featureset = npchunk_features(untagged_sent, i, history)
-                train_set.append( (featureset, tag) )
-                history.append(tag)
-        self.classifier = nltk.MaxentClassifier.train(
-            train_set, trace=0)
-
-    def tag(self, sentence):
-        history = []
-        for i, word in enumerate(sentence):
-            featureset = npchunk_features(sentence, i, history)
-            tag = self.classifier.classify(featureset)
-            history.append(tag)
-        return zip(sentence, history)
-
-
-
-class ConsecutiveNPChunker(nltk.ChunkParserI):
-    def __init__(self, train_sents):
-        tagged_sents = [[((w,t),c) for (w,t,c) in
-                         nltk.chunk.tree2conlltags(sent)]
-                        for sent in train_sents]
-        self.tagger = ConsecutiveNPChunkTagger(tagged_sents)
+        train_data = [[(t,c) for w,t,c in nltk.chunk.tree2conlltags(sent)]
+                      for sent in train_sents]
+        self.tagger = nltk.UnigramTagger(train_data)
 
     def parse(self, sentence):
-        tagged_sents = self.tagger.tag(sentence)
-        conlltags = [(w,t,c) for ((w,t),c) in tagged_sents]
+        pos_tags = [pos for (word,pos) in sentence]
+        tagged_pos_tags = self.tagger.tag(pos_tags)
+        chunktags = [chunktag for (pos, chunktag) in tagged_pos_tags]
+        conlltags = [(word, pos, chunktag) for ((word,pos),chunktag)
+                     in zip(sentence, chunktags)]
         return nltk.chunk.conlltags2tree(conlltags)
-
-
 
 
 
@@ -124,8 +73,8 @@ def analyze_semantics(sentenceBlob):
     # return structure_tree
 
 
-    chunker = ConsecutiveNPChunker(train_sents)
-    print(chunker.evaluate(test_sents))
+    unigram_chunker = UnigramChunker(train_sents)
+    print(unigram_chunker.evaluate(test_sents))
 
 def main():
     for sentence in storyText.sentences: # split text into sentences
