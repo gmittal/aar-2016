2e98e887c48a5f21f5554d22384e5ce974604f32
gmittal <gautam@mittal.net>
2016-02-10 17:14:29 -0800

    more experiments with n-gram chunkers

--- text-illustrate.py
+++ text-illustrate.py
@@ -27,6 +27,7 @@ class BigramChunker(nltk.ChunkParserI):
     def __init__(self, train_sents):
         train_data = [[(t,c) for w,t,c in nltk.chunk.tree2conlltags(sent)]
                       for sent in train_sents]
+
         self.tagger = nltk.BigramTagger(train_data)
 
     def parse(self, sentence):
