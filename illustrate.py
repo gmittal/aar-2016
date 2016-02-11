4846e96b43e57ea0c84e0003de33c323d552a2be
gmittal <gautam@mittal.net>
2016-02-11 13:14:27 -0800

    udpates

--- illustrate.py
+++ illustrate.py
@@ -3,10 +3,19 @@
 # Written by Gautam Mittal
 # Mentor: Robert Cheung
 
-import textParse
+from os.path import join, dirname
+from dotenv import load_dotenv
+import text_parse as textEngine
+from image_search import BingSearchAPI
+
+SEARCH_KEY = os.environ.get("BING_SEARCH_KEY")
 
 def main():
-    print(textParse.extract("The quick brown fox jumped over the lazy dog. The story was amazing."))
+    query_string = "cat"
+    bing = BingSearchAPI(SEARCH_KEY)
+    params = {'ImageFilters':'"Face:Face"', '$format': 'json', '$top': 10, '$skip': 0}
+    print(bing.search('image',query_string,params).json()) # requests 1.0+
+    # print(textEngine.extract("The quick brown fox jumped over the lazy dog. The story was amazing."))
 
 if __name__ == "__main__":
     main()
