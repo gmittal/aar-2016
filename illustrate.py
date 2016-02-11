f690e1bf7c453b43ea16ea51b1be8d9b2ee3302a
gmittal <gautam@mittal.net>
2016-02-11 15:23:17 -0800

    lots and lots of experiments

--- illustrate.py
+++ illustrate.py
@@ -3,18 +3,25 @@
 # Written by Gautam Mittal
 # Mentor: Robert Cheung
 
+import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 import text_parse as textEngine
-from image_search import BingSearchAPI
+import google as Google
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 SEARCH_KEY = os.environ.get("BING_SEARCH_KEY")
 
 def main():
-    query_string = "cat"
-    bing = BingSearchAPI(SEARCH_KEY)
-    params = {'ImageFilters':'"Face:Face"', '$format': 'json', '$top': 10, '$skip': 0}
-    print(bing.search('image',query_string,params).json()) # requests 1.0+
+
+
+    # request = urllib3.Request(url)
+    # request.add_header('Authorization', credentialBing)
+    # requestOpener = urllib2.build_opener()
+    # response = requestOpener.open(request)
+
+    # results = json.load(response)
     # print(textEngine.extract("The quick brown fox jumped over the lazy dog. The story was amazing."))
 
 if __name__ == "__main__":
