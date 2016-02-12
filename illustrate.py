f1956067338a846a87b086503887ea72b215c74f
gmittal <gautam@mittal.net>
2016-02-11 16:51:29 -0800

    updates to image search

--- illustrate.py
+++ illustrate.py
@@ -2,19 +2,28 @@
 # Takes plaintext as input and illustrates interpretation
 # Written by Gautam Mittal
 # Mentor: Robert Cheung
+# Requires Node.js and Python 3.4.6
+# $ npm install && pip install -r requirements.txt
 
 import os
 from os.path import join, dirname
-from subprocess import call
+from subprocess import check_output as call
 from dotenv import load_dotenv
 import text_parse as textEngine
+import json
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 SEARCH_KEY = os.environ.get("BING_SEARCH_KEY")
 
 def main():
+    query = "brown fox"
+    r = call(["node", "image_search.js", SEARCH_KEY, query])
+    r = str(r)[2:][:-3]
+    print(r)
+    image_json = json.loads(r)
 
+    # print(image_json)
     # print(textEngine.extract("The quick brown fox jumped over the lazy dog. The story was amazing."))
 
 if __name__ == "__main__":
--- image_search.js
+++ image_search.js
@@ -1,4 +1,6 @@
 // This requires Node.js
+/* Runs image search query through Bing API,
+   grabs a list of 50 image URLs, and chooses a random one */
 // Dependency of AAR Project 2015-2016
 // Written by Gautam Mittal
 
@@ -6,9 +8,20 @@ var Bing = require('node-bing-api')({ accKey: process.argv[2] });
 
 Bing.images(process.argv[3], {
   imageFilters: {
-    size: 'medium',
-    style: 'graphics'
+    // style: 'graphics'
   }
 }, function(error, res, body){
-console.log(JSON.stringify(body));
+  if (error) {
+    console.log("ERR");
+    process.exit();
+  } else {
+      var urls = [];
+      for (var i = 0; i < body["d"]["results"].length; i++) {
+        urls.push(body["d"]["results"][i].MediaUrl);
+      }
+
+      var randomIndex = Math.floor(Math.random() * urls.length) + 0;
+      console.log(urls[randomIndex]);
+  }
+
 });
