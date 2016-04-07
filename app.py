c92e6253a42ef23819de7253cbd1a7ae834700a5
Gautam Mittal <gautam@mittal.net>
2016-04-07 23:14:18 +0000

    updates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-# import illustrate
+from flask import Flask, render_template, request
+import illustrate
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
@@ -9,7 +9,8 @@ def home_main():
 # The magic endpoint
 @app.route("/illustrate", methods=["POST"])
 def illustrate():
-    return "hello"
+    if request.form['text_body']:
+        return illustrate.illustrate(request.form["text_body"])
 
 if __name__ == "__main__":
     app.run()
