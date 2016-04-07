20e93674cab82c017cc79c86caf3d1d614ddaf49
Gautam Mittal <gautam@mittal.net>
2016-04-07 22:39:36 +0000

    powering through this

--- illustrate.py
+++ illustrate.py
@@ -26,8 +26,6 @@ def getImageFromString(s):
     return(r)
 
 
-
-
 def generateGIF(file_names, size, uid):
     for fn in file_names:
         im = Image.open("./tmp_images/"+uid+"/"+fn)
@@ -44,63 +42,16 @@ def make_dir(path):
         if exception.errno != errno.EEXIST:
             raise
 
-def main():
+def illustrate(raw_text):
     make_dir("./tmp_images")
 
-    raw_text = """Once upon a time, there was a little girl named Goldilocks.  She  went for a walk in the forest.  Pretty soon, she came upon a house.  She knocked and, when no one answered, she walked right in.
-
-At the table in the kitchen, there were three bowls of porridge.  Goldilocks was hungry.  She tasted the porridge from the first bowl.
-
-"This porridge is too hot!" she exclaimed.
-
-So, she tasted the porridge from the second bowl.
-
-"This porridge is too cold," she said
-
-So, she tasted the last bowl of porridge.
-
-"Ahhh, this porridge is just right," she said happily and she ate it all up.
-
-After she'd eaten the three bears' breakfasts she decided she was feeling a little tired.  So, she walked into the living room where she saw three chairs.  Goldilocks sat in the first chair to rest her feet.
-
-"This chair is too big!" she exclaimed.
-
-So she sat in the second chair.
-
-"This chair is too big, too!"  she whined.
-
-So she tried the last and smallest chair.
-
-"Ahhh, this chair is just right," she sighed.  But just as she settled down into the chair to rest, it broke into pieces!
-
-Goldilocks was very tired by this time, so she went upstairs to the bedroom.  She lay down in the first bed, but it was too hard.  Then she lay in the second bed, but it was too soft.  Then she lay down in the third bed and it was just right.  Goldilocks fell asleep.
-
-As she was sleeping, the three bears came home.
-
-"Someone's been eating my porridge," growled the Papa bear.
-
-"Someone's been eating my porridge," said the Mama bear.
-
-"Someone's been eating my porridge and they ate it all up!" cried the Baby bear.
-
-"Someone's been sitting in my chair," growled the Papa bear.
-
-"Someone's been sitting in my chair," said the Mama bear.
-
-"Someone's been sitting in my chair and they've broken it all to pieces," cried the Baby bear.
-
-They decided to look around some more and when they got upstairs to the bedroom, Papa bear growled, "Someone's been sleeping in my bed,"
-
-"Someone's been sleeping in my bed, too" said the Mama bear
-
-"Someone's been sleeping in my bed and she's still there!" exclaimed Baby bear.
-
-Just then, Goldilocks woke up and saw the three bears.  She screamed, "Help!"  And she jumped up and ran out of the room.  Goldilocks ran down the stairs, opened the door, and ran away into the forest.  And she never returned to the home of the three bears.
-"""
-
     summary = summaryEngine.summarize(raw_text)
-    print summary
     svo = textEngine.extract(summary)
+
+    final_text_data = {
+        "summary": summary,
+        "svo_data": []
+    }
 
     for scene in svo:
         # print scene
@@ -116,34 +67,21 @@ Just then, Goldilocks woke up and saw the three bears.  She screamed, "Help!"  A
             # print "OBJECT"
             file_urls["object"] = getImageFromString(sent_object)
 
-        file_paths = []
+        sent_data = {
+            "subject": {
+                "text": sent_subject,
+                "image": file_urls["subject"]
+            },
+            "verb": {
+                "text": sent_predicate,
+                "image": file_urls["verb"]
+            },
+            "object": {
+                "text": sent_object,
+                "image": file_urls["object"] if len(sent_object) != 0 else None
+            }
+        }
 
-        u_id = str(uuid.uuid4())
-        make_dir("./tmp_images/"+u_id)
+        final_text_data["svo_data"].append(sent_data)
 
-        for class_id in file_urls:
-            print class_id
-            url = file_urls[class_id]
-            number = 0
-            if class_id == "subject":
-                number = 1
-            elif class_id == "verb":
-                number = 2
-            elif class_id == "object":
-                number = 3
-
-            extension = url.split(".")[len(url.split("."))-1]
-            dl.retrieve(url, "./tmp_images/"+u_id+"/"+str(number)+"."+extension)
-
-        # for n in os.listdir('./tmp_images/'+u_id):
-        #     im = Image.open('./tmp_images/'+u_id+"/"+n)
-        #     bg = Image.new("RGB", im.size, (255,255,255))
-        #     bg.paste(im,im)
-        #     bg.save("./tmp_images/"+u_id+"/"+str(n.split(".")[0])+".jpg", "JPEG")
-
-        file_paths = sorted((fn for fn in os.listdir('./tmp_images/'+u_id)))
-        print file_paths
-        generateGIF(file_paths, (400, 400), u_id)
-
-if __name__ == "__main__":
-    main()
+    return final_text_data
