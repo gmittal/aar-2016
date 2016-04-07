f4ecec85615fb1974aaa1d66ecbaf2dbbc27eed3
Gautam Mittal <gautam@mittal.net>
2016-04-07 23:10:59 +0000

    updates

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-import illustrate
+from flask import Flask, render_template
+# import illustrate
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home_main():
+    return render_template('index.html')
+
+# The magic endpoint
+@app.route("/illustrate", methods=["POST"])
+def illustrate():
+    return "hello"
+
+if __name__ == "__main__":
+    app.run()
